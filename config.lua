54be668d161b7f421b405d66ba4cd2b362f0c095
Alburrida <alburrida@gmail.com>
2024-05-13 21:38:45 +0200

    mas codigo

--- config.lua
+++ config.lua
@@ -9,3 +9,18 @@ suma = numero1 + numero2
 resta = numero1 - numero2
 multiplicacion = numero1 * numero2
 division = numero1 / numero2
+
+-- ejercicio 2
+limite = 33
+pares = 0
+impares = 0
+
+for i = 1, limite do
+    if i % 2 == 0 then
+        -- Si el número es par
+        pares = pares + 1
+    else
+        -- Si el número es impar
+        impares = impares + 1
+    end
+end
