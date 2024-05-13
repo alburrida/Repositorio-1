e2130fc84c452ad91af8a6c454bc59e26e486f5d
Alburrida <alburrida@gmail.com>
2024-05-13 21:29:47 +0200

    primer archivo

---
+++ config.lua
@@ -0,0 +1,11 @@
+-- (ɔ◔‿◔)ɔ ♥
+
+-- ejercicio 1
+numero1 = 10
+numero2 = 5
+
+-- operaciones
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
